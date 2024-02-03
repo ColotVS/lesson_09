249c6521c23f0f001b03e4444b8d6985ad3d954d
ColotVS <kolotilov98@mail.ru>
2024-02-03 14:42:33 +0700

    Случайное число от 0 до 99 Урок 9, задача 8

---
+++ lesson_09_08.java
@@ -0,0 +1,11 @@
+public class lesson_09_08 {
+    //В этой задаче тебе предстоит реализовать метод generateNumber(), который будет возвращать случайное число от 0 до 99.
+    //В методе generateNumber() используй метод Math.random().
+    public static void main(String[] args) {
+        System.out.println(generateNumber());
+    }
+
+    public static int generateNumber() {
+        return (int) (Math.random()*100);
+    }
+}
